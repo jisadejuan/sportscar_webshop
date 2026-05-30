21c786ded50a7be90bb928a16f1fe6b8ea19ed52
jisadejuan <dejuanjessa700@gmail.com>
2026-05-31 06:42:11 +0800

    Update product.py

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, render_template
+from app.models.product import Product
 
+product_bp = Blueprint('product', __name__)
+
+@product_bp.route('/products')
+def product_list():
+    products = Product.query.all()
+    return render_template('products/list.html', products=products)
+
+@product_bp.route('/products/<int:product_id>')
+def product_detail(product_id):
+    product = Product.query.get(product_id)
+    return render_template('products/detail.html', product=product)
