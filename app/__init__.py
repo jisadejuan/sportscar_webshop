f440000fba5184edf96336a700dfebebb9b77638
jisadejuan <dejuanjessa700@gmail.com>
2026-05-31 06:39:23 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,21 @@
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/carshop_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from app.controllers.product import product_bp
+    app.register_blueprint(product_bp)
+
+    return app
