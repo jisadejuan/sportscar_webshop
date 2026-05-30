2c936a34c1b48326ece451e64ac600c836a5e24a
jisadejuan <dejuanjessa700@gmail.com>
2026-05-31 06:56:48 +0800

    Update product.py

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -3,6 +3,26 @@ from app.models.product import Product
 
 product_bp = Blueprint('product', __name__)
 
+@product_bp.route('/')
+def home():
+    return render_template('public/index.html')
+
+@product_bp.route('/about')
+def about():
+    return render_template('public/about.html')
+
+@product_bp.route('/contact')
+def contact():
+    return render_template('public/contact.html')
+
+@product_bp.route('/login')
+def login():
+    return render_template('public/login.html')
+
+@product_bp.route('/categories')
+def categories():
+    return render_template('public/categories.html')
+
 @product_bp.route('/products')
 def product_list():
     products = Product.query.all()
