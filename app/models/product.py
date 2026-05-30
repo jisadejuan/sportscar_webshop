b3d3a65a5e4053a348d235a427aa4ed03159953e
jisadejuan <dejuanjessa700@gmail.com>
2026-05-31 06:41:16 +0800

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -1,1 +1,15 @@
+from app import db
 
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.String(50), nullable=False)
+    stock = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
