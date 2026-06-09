b46b9a02e79a431f5fd1d2ac6453e4ccb9f71689
jisadejuan <dejuanjessa700@gmail.com>
2026-06-09 08:46:56 +0800

    Update product.py

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -34,10 +34,11 @@ def product_list():
         categories.setdefault(car.category, []).append(car)
     return render_template('products/list.html', categories=categories)
 
-@product_bp.route('/admin/products')
+@product_bp.route('/admin')
 def admin_dashboard():
     products = Product.query.all()
-    return render_template('admin/products/admin.html', products=products)
+    return render_template('admin/admin.html', products=products)
+    
 @product_bp.route('/products/<int:product_id>')
 def product_detail(product_id):
     product = Product.query.get_or_404(product_id)
