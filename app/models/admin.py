bd5ca9164ffeffb37cd639428eb5d59bd4a2756d
jisadejuan <dejuanjessa700@gmail.com>
2026-06-14 19:44:09 +0800

    Update admin.py

--- app/models/admin.py
+++ app/models/admin.py
@@ -5,6 +5,3 @@ class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-
-    def __repr__(self):
-        return f"<Admin {self.email}>"
