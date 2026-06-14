5452b70ac4feb9cdcd1744599278d62e89ca7fd3
jisadejuan <dejuanjessa700@gmail.com>
2026-06-14 19:20:18 +0800

    Update and rename admin_user.py to admin.py

--- app/models/admin.py
+++ app/models/admin.py
@@ -1,6 +1,10 @@
 from app import db
 
-class AdminUser(db.Model):
+class Admin(db.Model):
+    __tablename__ = 'admins'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"<Admin {self.email}>"
