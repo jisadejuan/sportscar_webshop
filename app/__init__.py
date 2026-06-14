1a342a1c80375609636890c0ccdbf14691704ec7
jisadejuan <dejuanjessa700@gmail.com>
2026-06-14 10:46:56 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,51 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-# Two separate SQLAlchemy instances
-db_user = SQLAlchemy()
-db_admin = SQLAlchemy()
-
-migrate_user = Migrate()
-migrate_admin = Migrate()
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
 
-    # User database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/carshop_db'
-    db_user.init_app(app)
-    migrate_user.init_app(app, db_user)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Admin database (separate URI)
-    app.config['SQLALCHEMY_DATABASE_ADMIN_URI'] = 'mysql+pymysql://root:@localhost/admin_db'
-    db_admin.init_app(app)
-    migrate_admin.init_app(app, db_admin)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-    # Import models so migrations can detect them
+    # Import models so Flask-Migrate can detect them
     from app.models import product
-    from app.models_admin import admin_product
 
     # Register controllers
     from app.controllers.product import product_bp
-    from app.controllers.admin import admin_bp
     app.register_blueprint(product_bp)
-    app.register_blueprint(admin_bp)
 
     return app
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/carshop_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Import models so Flask-Migrate can detect them
+    from app.models import product
+
+    # Register controllers
+    from app.controllers.product import product_bp
+    app.register_blueprint(product_bp)
+
+    return app
+
