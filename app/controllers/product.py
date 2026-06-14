6a348f44e05f8db4d938225412dd6b9e9657b745
jisadejuan <dejuanjessa700@gmail.com>
2026-06-14 15:08:12 +0800

    Update product.py

--- app/controllers/product.py
+++ app/controllers/product.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 from app.models.product import Product
-from app import db
 
 product_bp = Blueprint('product', __name__)
 
@@ -44,26 +43,3 @@ def product_detail(product_id):
             categories.setdefault(car.category, []).append(car)
         return render_template('products/list.html', categories=categories)
     return render_template('products/detail.html', product=product)
-
-@product_bp.route('/products/<int:product_id>/delete', methods=['GET', 'POST'])
-def product_delete(product_id):
-    product = Product.query.get(product_id)
-    if product is None:
-        # Kung wala ang product, balik sa list
-        products = Product.query.all()
-        categories = {}
-        for car in products:
-            categories.setdefault(car.category, []).append(car)
-        return render_template('products/list.html', categories=categories)
-
-    if request.method == 'POST':
-        db.session.delete(product)
-        db.session.commit()
-        # Pagkatapos mag-delete, ipakita ulit ang list
-        products = Product.query.all()
-        categories = {}
-        for car in products:
-            categories.setdefault(car.category, []).append(car)
-        return render_template('products/list.html', categories=categories)
-
-    return render_template('admin/confirm_delete.html', product=product)
