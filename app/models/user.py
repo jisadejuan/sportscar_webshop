22ed01ac5257dd19e7022acc4e8b77395914c38d
jisadejuan <dejuanjessa700@gmail.com>
2026-06-15 11:52:27 +0800

    Delete app/models/user.py

--- app/models/user.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class User(db.Model):
-    __tablename__ = 'users'   # plural para malinaw
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
